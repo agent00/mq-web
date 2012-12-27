febede5e4a173c11d23f32699942fa561dd1a2a2
Martin Blech <martinblech@gmail.com>
2012-12-27 19:20:57 -0300

    add log4j dependency (required by kafka)

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,9 @@ object ApplicationBuild extends Build {
 
   val appName         = "mq-web"
   val appVersion      = "0.1"
-  val appDependencies = Seq()
+  val appDependencies = Seq(
+    "log4j" % "log4j" % "1.2.17"
+  )
 
   lazy val main = PlayProject(appName, appVersion, appDependencies)
 
